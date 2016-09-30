e8c44e5f437b1547360650f7b76704e09eaef8d6
Денис <>
2016-09-30 18:36:45 +0900

    ertyui

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -25,21 +25,7 @@ class AdminController extends Controller
         $this->middleware(['auth', 'AdminRoleCheck']);
         $this->request = $request;
 
-        echo "dwdw";
-        echo 12312312312;
-        echo 12312312312;
-        echo 12312312312;
-        echo 12312312312;
-        echo 12312312312;
-        echo 12312312312;
-        echo 12312312312;
-
-        echo 21;
-
-        echo 123456789456132684;
-
-        echo "hello world";
-
+       echo 23;
 
     }
 
